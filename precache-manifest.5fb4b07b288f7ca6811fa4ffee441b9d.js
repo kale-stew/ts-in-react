6115dec515dddc6e6702352f68d65e0cc7161089
Kylie Stewart <kylieastewart@gmail.com>
2019-04-16 16:40:47 -0600

    Updates

---
+++ precache-manifest.5fb4b07b288f7ca6811fa4ffee441b9d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfb1e2e5b6a80fc7fb8d95fa0503f1f8",
+    "url": "/ts-in-react/static/media/intellisense.dfb1e2e5.gif"
+  },
+  {
+    "revision": "d9af47b6603edbeee70c",
+    "url": "/ts-in-react/static/css/main.15d84846.chunk.css"
+  },
+  {
+    "revision": "af96f3861afd83e81a07",
+    "url": "/ts-in-react/static/js/runtime~main.54005517.js"
+  },
+  {
+    "revision": "09dfff618f0cff650934",
+    "url": "/ts-in-react/static/js/2.f90cadc8.chunk.js"
+  },
+  {
+    "revision": "b40cbdd45c51a539183dcfdb465c31e9",
+    "url": "/ts-in-react/static/media/clippy.b40cbdd4.jpg"
+  },
+  {
+    "revision": "b181a08a7b14d7a6b7fbc4ff84732e3e",
+    "url": "/ts-in-react/static/media/clippy-tweet.b181a08a.jpg"
+  },
+  {
+    "revision": "8cae3f23bacca2e6f5fc37949ef6db45",
+    "url": "/ts-in-react/static/media/new-formidable.8cae3f23.png"
+  },
+  {
+    "revision": "d9af47b6603edbeee70c",
+    "url": "/ts-in-react/static/js/main.d767071f.chunk.js"
+  },
+  {
+    "revision": "cfdc163035342f71d33e482de3f76df7",
+    "url": "/ts-in-react/static/media/the-other-girl.cfdc1630.jpg"
+  },
+  {
+    "revision": "1153020eec39cc71755dbc6b1db45ef7",
+    "url": "/ts-in-react/static/media/refactoring.1153020e.png"
+  },
+  {
+    "revision": "a05785b061b9ca51b59359325f02f72d",
+    "url": "/ts-in-react/static/media/react-with-ts.a05785b0.png"
+  },
+  {
+    "revision": "3f981acdd2c7c5959db6de937efb4aea",
+    "url": "/ts-in-react/static/media/vs-code.3f981acd.png"
+  },
+  {
+    "revision": "a3462b4e7c91e6bbc3b0c4862f11d49b",
+    "url": "/ts-in-react/static/media/warning.a3462b4e.png"
+  },
+  {
+    "revision": "38416e8c7ce50792371e1129984581fd",
+    "url": "/ts-in-react/index.html"
+  }
+];
